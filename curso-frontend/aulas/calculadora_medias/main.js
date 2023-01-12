58e683839ea338fb487bf344a38d7ae2772756b3
LucasLimaDM <lucaslim.dias@gmail.com>
2023-01-12 15:14:46 -0300

    Update prompt de nota e impedir duplicatas

--- curso-frontend/aulas/calculadora_medias/main.js
+++ curso-frontend/aulas/calculadora_medias/main.js
@@ -3,6 +3,7 @@ const tbody = document.querySelector('table tbody')
 const tfoot = document.querySelector('table tfoot')
 const nomeAtividade = document.querySelector('#nome-ativ')
 const notaAtividade = document.querySelector('#nota-ativ')
+const notaMinima = parseFloat(prompt('Digite a média para aprovação: '))
 var numeroAtividades = 0
 var numeroNotas = 0
 var mediaDasNotas = 0
@@ -14,6 +15,11 @@ form.addEventListener('submit', function(e){
 
     // desabilita a função padrão do submit
     e.preventDefault()
+
+    if (atividades.includes(nomeAtividade.value)) {
+        alert('Essa atividade já foi inserida')
+    } else {
+
     // adiciona a linha à tabela
     adicionaLinha()
     // envia os dados para armazenamento em array
@@ -21,13 +27,13 @@ form.addEventListener('submit', function(e){
     notas.push(Number(notaAtividade.value))
     // atualiza o foot da tabela com a média final e o botão aprovado
     atualizaTfoot()
-    
+    }
 })
 
 
 function adicionaLinha() {
     // adiciona a linha à tabela
-    let linhaAMais = `<tr> <td>${nomeAtividade.value}</td> <td>${notaAtividade.value}</td> <td> <img src="${notaAtividade.value >= 7 ? 'images/aprovado.png' : 'images/reprovado.png' }" alt="${notaAtividade.value >= 7 ? 'Emoji festejando' : 'Emoji triste' }"></td></tr>`
+    let linhaAMais = `<tr> <td>${nomeAtividade.value}</td> <td>${notaAtividade.value}</td> <td> <img src="${notaAtividade.value >= notaMinima ? 'images/aprovado.png' : 'images/reprovado.png' }" alt="${notaAtividade.value >= notaMinima ? 'Emoji festejando' : 'Emoji triste' }"></td></tr>`
     tbody.innerHTML += linhaAMais
 }
 
@@ -41,11 +47,11 @@ function atualizaTfoot() {
     } 
     mediaDasNotas = somaDasNotas / notas.length
     
-    tfoot.innerHTML = `<td>Média final</td><td>${mediaDasNotas}</td><td> <span class="resultado">${mediaDasNotas >= 7 ? 'Aprovado' : 'Reprovado'}</span></td>`
+    tfoot.innerHTML = `<td>Média final</td><td>${mediaDasNotas.toFixed(2)}</td><td> <span class="resultado">${mediaDasNotas >= notaMinima ? 'Aprovado' : 'Reprovado'}</span></td>`
 
     // atualiza a classe do botão aprovado
     let resultado = document.querySelector('.resultado')
-    if (mediaDasNotas >= 7) {
+    if (mediaDasNotas >= notaMinima) {
         resultado.classList.add('aprovado')
         resultado.classList.remove('reprovado')
     }else {
