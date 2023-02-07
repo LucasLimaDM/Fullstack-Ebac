d61154b28b68b1e4f670ae895be0e862af6761b5
LucasLimaDM <lucaslim.dias@gmail.com>
2023-02-07 09:05:25 -0300

    Renomeação de arquivos

---
+++ INFNET-frontend/book-inspirations/css-tries/script.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('form')
+const formulary = document.querySelector('.formulary')
+formulary.style.display = 'flex'
+form.style.display = 'flex'
+console.log(formulary)
