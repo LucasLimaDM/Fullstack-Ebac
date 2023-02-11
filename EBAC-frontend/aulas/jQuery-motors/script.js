3488c1090dadb251dad602bf50fc3c656bd0c938
LucasLimaDM <lucaslim.dias@gmail.com>
2023-02-11 19:17:54 -0300

    Inicio EBAC Motors

---
+++ EBAC-frontend/aulas/jQuery-motors/script.js
@@ -0,0 +1,5 @@
+let ul = document.querySelector('ul')
+let p = document.querySelector('p')
+const links = document.querySelectorAll('a')
+    
+// chega na medida, cria o sidebar invisivel, ele recebe a ul, depois jquery puxa pro lado
