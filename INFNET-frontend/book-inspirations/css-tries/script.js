eef6f09b4dc17dd167b98c1f650bd037852f7235
LucasLimaDM <lucaslim.dias@gmail.com>
2023-03-02 21:50:10 -0300

    adiçoes em diferentes projetos

--- INFNET-frontend/book-inspirations/css-tries/script.js
+++
@@ -1,7 +0,0 @@
-// criar um novo elemento dentro do p e dar a ele configurações
-const p = document.querySelector('p')
-const a = document.createElement('a')
-a.innerText = 'Clique ai'
-a.href = '#'
-p.appendChild(a)
-console.log()
