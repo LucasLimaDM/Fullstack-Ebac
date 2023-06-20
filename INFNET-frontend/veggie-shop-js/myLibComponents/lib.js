f879a4ee26ce8c6bd81836a25b482cbde8378830
LucasLDias <lucaslim.dias@gmail.com>
2023-06-20 18:35:35 -0300

    commit para teste na marcação da check mark-all

--- INFNET-frontend/veggie-shop-js/myLibComponents/lib.js
+++ INFNET-frontend/veggie-shop-js/myLibComponents/lib.js
@@ -22,62 +22,150 @@ window.onresize = ()=> { menuBar.classList.remove('open'); }
 
 //radio-button e checkbox
 
-const radioBtns = document.querySelectorAll('.radio-btn');
+const radioButtons = document.querySelectorAll('.radio-btn');
 
+radioButtons.forEach(radioButton => {
+    radioButton.addEventListener('click', ev => {
+        const radioButton = ev.target;
+        const sameNameButtons = document.querySelectorAll(`.radio-btn[name=${radioButton.attributes.name.value}]`);
+        sameNameButtons.forEach(sameNameButton => sameNameButton.classList.remove('checked'));
+        radioButton.classList.toggle('checked');
+    });
+});
 
-const activeBtns = document.querySelectorAll('.radio-btn, .checkbox-btn');
+//checkbox
 
-activeBtns.forEach(activeBtn => {
-    function activeBase(){
-        
-        if(!activeBtn.classList.contains('checked') && activeBtn.classList.contains('radio-btn')){        
+const checkMarkAll = document.querySelectorAll('.checkbox-btn.mark-all');
+const checkMarkOne = document.querySelectorAll('.checkbox-btn.mark-one');
 
-            const sameNameBtns = document.querySelectorAll(`.radio-btn[name=${activeBtn.attributes.name.value}]`);
+checkMarkOne.forEach(checkbox => {
+    checkbox.addEventListener('click', ()=>{ clickCheckbox(checkbox) });
 
-            sameNameBtns.forEach(btn => { btn.classList.remove('checked') });
+    const label = document.querySelector(`label[for=${checkbox.id}]`);
 
-        } else if(activeBtn.classList.contains('checkbox-btn')){
-            /*Para todas as mark-ones pois as mark-alls vão para a outra função*/
+    label.addEventListener('click', ()=>{ clickCheckbox(checkbox) });
+});
 
-            const checks = [];
-            otherSameNameChecks.forEach(el => { checks.push(el.classList.contains('checked')) });
+checkMarkAll.forEach(checkbox => {
+    checkbox.addEventListener('click', () => {
+        const checkName = checkbox.attributes.name.value;
+        const sameNameChecks = document.querySelectorAll(`.checkbox-btn[name=${checkName}].mark-one`)
+        if(checkbox.classList.contains('mark')){
+            sameNameChecks.forEach(checkbox => { clickCheckbox(checkbox) });
+        } else{
 
-            if(checks.some()){
-
-            }
-        }
-        activeBtn.classList.toggle('checked');
     }
+    })
 
-    function marcaTodos(){
+});
 
-        const otherSameNameChecks = document.querySelectorAll(`.checkbox-btn[name=${activeBtn.attributes.name.value}.mark-one]`);
 
-        const checks = [];
 
-        otherSameNameChecks.forEach(el => {
-            checks.push(el.classList.contains('checked'))
+function clickCheckbox(checkbox){
+    checkbox.classList.toggle('checked');
+    atualizaMarkAll(checkbox.attributes.name.value);
+}
+
+function atualizaMarkAll(checkName){
+    console.log('checkName :', checkName);
+    const checkOnes = [
+        ...document.querySelectorAll(`.checkbox-btn[name=${checkName}].mark-one`)
+    ]
+    .map(check => check.classList.contains('checked'));
+
+    const checkAll = document.querySelectorAll(`.checkbox-btn[name=${checkName}].mark-all`)
+
+    if(checkOnes.every(b=>b===false)){
+        checkAll.forEach(checkbox => {
+            checkbox.classList.add('mark');
+            checkbox.classList.remove('dismark', 'checked', 'half');
+
+            
         });
-        
-        if(checks.includes(true)){
-            otherSameNameChecks.forEach(checkbox => {
-                checkbox.classList.remove('checked');
-            })
-        } else {
-            otherSameNameChecks.forEach(checkbox => {
-                checkbox.classList.add('checked');
-            });
-        }           
+    } else if(checkOnes.every(b=>b===true)){
+        checkAll.forEach(checkbox => {
+            checkbox.classList.add('dismark', 'checked');
+            checkbox.classList.remove('mark', 'half');
+            
+        });
+    } else if(!checkOnes.every(b=>b===true) && !checkOnes.every(b=>b===false)){
+        checkAll.forEach(checkbox => {
+            checkbox.classList.add('half', 'checked');
+            checkbox.classList.remove('mark', 'dismark');
+            // checkbox.classList.remove('dismark');
+        });
     }
+}
 
 
 
-    const funcAtiva = activeBtn.classList.contains('mark-all') ? marcaTodos : activeBase; 
 
-    const label = document.querySelector(`label[for=${activeBtn.id}]`);
-    label.addEventListener('click', funcAtiva, activeBtn);
-    activeBtn.addEventListener('click', funcAtiva, activeBtn);
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const activeBtns = document.querySelectorAll('.radio-btn, .checkbox-btn');
+
+// activeBtns.forEach(activeBtn => {
+//     function activeBase(){
+        
+//         if(!activeBtn.classList.contains('checked') && activeBtn.classList.contains('radio-btn')){        
+
+//             const sameNameBtns = document.querySelectorAll(`.radio-btn[name=${activeBtn.attributes.name.value}]`);
+
+//             sameNameBtns.forEach(btn => { btn.classList.remove('checked') });
+
+//         } else if(activeBtn.classList.contains('checkbox-btn')){
+//             /*Para todas as mark-ones pois as mark-alls vão para a outra função*/
+
+//             const checks = [];
+
+//             if(checks.some()){
+
+//             }
+//         }
+//         activeBtn.classList.toggle('checked');
+//     }
+
+//     function marcaTodos(){
+
+//         const otherSameNameChecks = document.querySelectorAll(`.checkbox-btn[name=${activeBtn.attributes.name.value}.mark-one]`);
+
+//         const checks = [];
+
+//         otherSameNameChecks.forEach(el => {
+//             checks.push(el.classList.contains('checked'))
+//         });
+        
+//         if(checks.includes(true)){
+//             otherSameNameChecks.forEach(checkbox => {
+//                 checkbox.classList.remove('checked');
+//             })
+//         } else {
+//             otherSameNameChecks.forEach(checkbox => {
+//                 checkbox.classList.add('checked');
+//             });
+//         }           
+//     }
+
+
+
+//     const funcAtiva = activeBtn.classList.contains('mark-all') ? marcaTodos : activeBase; 
+
+//     const label = document.querySelector(`label[for=${activeBtn.id}]`);
+//     label.addEventListener('click', funcAtiva, activeBtn);
+//     activeBtn.addEventListener('click', funcAtiva, activeBtn);
+// })
 
 //select 
 const selectBtns = document.querySelectorAll('.select-btn');
