0b556b815fd47d87554018a9af511b2dbf2ba346
LucasLDias <lucaslim.dias@gmail.com>
2023-08-17 21:09:11 -0300

    Revisão de aula sobre Deque e Queue

--- EBAC-backend/aulas/aula_Deque/src/Main.java
+++ EBAC-backend/aulas/aula_Deque/src/Main.java
@@ -1,2 +1,46 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+
 public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Deque ArrayDeque");
+
+
+        Deque<String> d = new ArrayDeque<String>();
+
+
+        d.add("Red");
+        d.add("Yellow");
+        d.add("Blue");
+        d.add("Green");
+
+        for(int i = 0; i < 4; i++){
+            System.out.println(d.remove());
+        }
+
+        System.out.println("Queue ArrayDeque");
+
+
+        Queue<String> arrQueue = new ArrayDeque<String>();
+
+
+        arrQueue.add("Red");
+        arrQueue.add("Yellow");
+        arrQueue.add("Blue");
+        arrQueue.add("Green");
+
+        for(int i = 0; i < 4; i++){
+            System.out.println(arrQueue.remove());
+        }
+
+
+
+
+
+
+
+    }
+
 }
