828578283a9a6b15ffa242b9aa5586bd2a1344bc
LucasLDias <lucaslim.dias@gmail.com>
2023-08-17 21:11:37 -0300

    Criação projeto festa junina para exercicio ebac

---
+++ EBAC-backend/exercicios/tarefa_ColecoesParte1/src/br/com/lucasdias/Main.java
@@ -0,0 +1,87 @@
+package br.com.lucasdias;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        //
+        Set<Participante> participantesMasculinos = new HashSet<>();
+        Set<Participante> participantesFemininos = new HashSet<>();
+
+
+        System.out.println("------   Bem vindo ao cadastro de participantes da festa junina!   -------");
+        System.out.println("------   Os participantes serão separados pelo sexo   -------");
+
+        navigator(participantesMasculinos, participantesFemininos);
+
+        System.out.println("------   Obrigado por usar o cadastro da festa junina!   ------");
+    }
+
+    public static void navigator(Set<Participante> participantesMasculinos, Set<Participante> participantesFemininos){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" ");
+        System.out.println("Se deseja cadastrar um novo participante, digite cadastro");
+        System.out.println("Se deseja visualizar os participantes cadastrados, digite visualizar");
+        System.out.println("Se deseja encerrar o programa, pressione qualquer tecla");
+
+        String comando = sc.next();
+        sc.nextLine();
+        if(comando.equals("cadastro")){
+            cadastroParticipante(participantesMasculinos, participantesFemininos);
+        } else if(comando.equals("visualizar")){
+            visualizarParticipantes(participantesMasculinos);
+            visualizarParticipantes(participantesFemininos);
+            navigator(participantesMasculinos, participantesFemininos);
+        }
+    }
+
+    public static void cadastroParticipante(Set<Participante> participantesMasculinos, Set<Participante> participantesFemininos){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("cadastrando...");
+
+        System.out.print("Digite o nome do(a) participante: ");
+        String nome = sc.nextLine();
+        System.out.println(" ");
+
+        System.out.print("Digite o sexo do(a) participante: ");
+        String sexo = sc.nextLine();
+        System.out.println(" ");
+
+
+        if(sexo.equals("masculino")){
+            participantesMasculinos.add(new Participante(nome, sexo));
+        } else {
+            participantesFemininos.add(new Participante(nome, sexo));
+        }
+
+
+        navigator(participantesMasculinos, participantesFemininos);
+    }
+
+
+    public static void visualizarParticipantes(Set<Participante> participantes){
+
+        participantes.forEach(participante -> {
+            System.out.println("================================");
+            System.out.println(" ");
+
+            System.out.print("Nome: " + participante.getNome());
+            System.out.println(" ");
+
+            System.out.print("Sexo: " + participante.getSexo());
+            System.out.println(" ");
+
+            System.out.println(" ");
+            System.out.println("================================");
+
+
+        });
+    }
+}
---
+++ EBAC-backend/exercicios/tarefa_ColecoesParte1/src/br/com/lucasdias/Participante.java
@@ -0,0 +1,38 @@
+package br.com.lucasdias;
+
+public class Participante implements Comparable<Participante>{
+
+    private String nome;
+
+    private String sexo;
+
+    public Participante(String nome, String sexo){
+        this.sexo = sexo;
+        this.nome = nome;
+    }
+
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+
+    @Override
+    public int compareTo(Participante part) {
+        return this.nome.compareTo(part.getNome());
+    }
+}
